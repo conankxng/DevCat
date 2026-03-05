e046ed34d7d5368df93004b4b48f59de66be516c
sillybento <neocheetahlee@gmail.com>
2026-03-06 02:48:53 +0700

    zzzz

--- ui_report.py
+++ ui_report.py
@@ -1,1 +1,10 @@
 import tkinter as tk
+from tkinter import ttk, messagebox #ttk คือ widget  messagebox คือหน้าต่างแจ้งเตือนpopup
+import product_manager as pm
+
+def financial_summary(): 
+    try: 
+        revenue = report.total_revenue() 
+        expense = report.total_expense() 
+        profit = revenue - expense
+    
