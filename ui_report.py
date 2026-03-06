b47e66994a7da4b5f104712456f0f7619153699c
Apirak_TMK <aomapirak1122@gmail.com>
2026-03-07 01:15:01 +0700

    404

--- ui_report.py
+++ ui_report.py
@@ -8,5 +8,5 @@ def setup_Report_interface(parent):
     default_font = ("Kanit", 12)  #เก็บตัวแปรฟอนต์แบบปกติ
     bold_font = ("Kanit", 14, "bold") #เก็บตัวแปรฟอนต์แบบตัวหนา
 
-    revenue_frame = tk.Frame(parent,background="#FF9EE4")
+    revenue_frame = tk.Frame(parent, background="#FF9EE4")
     revenue_frame
