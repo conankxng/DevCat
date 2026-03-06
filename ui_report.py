a21071ef65b93989c4593ffa97d9e560e299bbda
sillybento <neocheetahlee@gmail.com>
2026-03-07 00:51:34 +0700

    ใใใ

--- ui_report.py
+++ ui_report.py
@@ -1,1 +1,14 @@
 import tkinter as tk
+from tkinter import ttk, messagebox
+import report
+
+#ฟังก์ชันสำหรับสร้างหน้าต่าง Reportโดยแสดงผลใน (p3)
+def setup_Report_interface(parent): 
+
+    default_font = ("Kanit", 12)  #เก็บตัวแปรฟอนต์แบบปกติ
+    bold_font = ("Kanit", 14, "bold") #เก็บตัวแปรฟอนต์แบบตัวหนา
+
+    revenue_frame = tk.Frame(parent,background="#FF9EE4")
+
+
+
